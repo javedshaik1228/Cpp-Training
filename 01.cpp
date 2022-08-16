3546e8c24121515a1000ff9fe83c1105b8eac63a
javedshaik1228 <89538495+javedshaik1228@users.noreply.github.com>
2022-08-16 15:11:43 +0530

    Create 01.cpp
    write a program to accept principle,rate of interest and time calculate simple interest and display the same.

---
+++ 01.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int p,t;
+    double r;
+    cin>>p>>r>>t;
+    double si=(p*t*r)/100;
+    cout<<si<<endl;
+
+    return 0;
+}
